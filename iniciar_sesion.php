cfe6b62a3b03c3529be013adafbe0ed38fc061a6
Jorge Flores <jorgeafa.19@gmail.com>
2026-04-24 21:52:57 -0500

    ✨ feat(auth): agregar autenticación de usuarios

---
+++ iniciar_sesion.php
@@ -0,0 +1,62 @@
+<?php
+include_once 'funciones/conexion.php';
+include_once 'funciones/clientes/clientes_sesion.php';
+?>
+
+<!DOCTYPE html>
+<html lang="ES">
+
+<head>
+    <title>Inicio de sesión</title>
+    <?php require_once './fragments/links.php'; ?>
+</head>
+
+<body class="fondo-login">
+    <?php require_once './fragments/header.php'; ?>
+    <main>
+        <div class="container">
+            <section class="jumbo">
+                <article class="flex-center">
+                    <div class="tarjeta jumbo-info">
+                        <form id="loginForm" class="formulario" action="iniciar_sesion.php" method="POST">
+                            <fieldset>
+                                <legend>Iniciar sesión</legend>
+                                <label>Usuario</label>
+                                <input type="text" id="usuario" name="usuario" placeholder="Ingrese su usuario" maxlength="50">
+
+                                <label>Contraseña</label>
+                                <input type="password" id="clave" name="clave" placeholder="Ingrese su clave" maxlength="50">
+                                
+                                <button class="submit-button" type="submit">Iniciar</button>
+                                <button class="submit-button" type="button">
+                                    <a href="registro.php">Regístrate</a>
+                                </button>
+                            </fieldset>
+                        </form>
+                </article>
+            </section>
+        </div>
+    </main>
+    <script>
+        <?php if (isset($_GET['mensaje']) && $_GET['mensaje'] == 'error') : ?>
+            Swal.fire({
+                icon: 'error',
+                title: 'Datos incorrectos',
+                text: 'Por favor, verifique sus credenciales',
+            });
+        <?php elseif (isset($_GET['mensaje']) && $_GET['mensaje'] == 'exito') : ?>
+            Swal.fire({
+                icon: 'success',
+                title: 'Bienvenido',
+                text: 'Inicio de sesión exitoso',
+                showConfirmButton: false,
+                timer: 1300
+            }).then(function() {
+                window.location.href = 'intranet.php';
+            });
+        <?php endif; ?>
+    </script>
+    <?php require_once './fragments/footer.php'; ?>
+</body>
+
+</html>
---
+++ registro.php
@@ -0,0 +1,58 @@
+<?php
+include_once 'funciones/conexion.php';
+include_once 'funciones/clientes/clientes_registro.php';
+?>
+<!DOCTYPE html>
+<html lang="ES">
+
+<head>
+    <title>Registro de usuario</title>
+    <?php require_once './fragments/links.php'; ?>
+</head>
+
+<body class="fondo-registro">
+    <?php require_once './fragments/header.php'; ?>
+    <main>
+        <div class="container">
+            <section class="jumbo">
+                <article class="flex-center">
+                    <div class="tarjeta jumbo-info">
+                        <form class="formulario" action="" method="POST">
+                            <fieldset>
+                                <legend>Regístrate</legend>
+                                <input type="text" name="apellido" placeholder="Apellidos" maxlength="50" required>
+                                <input type="text" name="nombre" placeholder="Nombres" maxlength="50" required>
+                                <input type="email" name="correo" placeholder="Correo electronico" maxlength="200" required>
+                                <input type="text" name="usuario" placeholder="Nombre de usuario" maxlength="50" required>
+                                <input type="password" name="clave" placeholder="Contraseña" maxlength="50" required>
+                                <input type="password" name="clave_confirmar" placeholder="Confirmar contraseña" maxlength="50" required>
+                                <button class="submit-button" type="submit">Continuar</button>
+                            </fieldset>
+                        </form>
+                    </div>
+                </article>
+            </section>
+        </div>
+    </main>        
+    <script>
+        <?php if (isset($_GET['mensaje']) && $_GET['mensaje'] == 'false') : ?>
+            Swal.fire({
+                icon: 'error',
+                title: 'Complete los datos',
+                text: 'Por favor, inténtelo de nuevo',
+            });
+        <?php elseif (isset($_GET['mensaje']) && $_GET['mensaje'] == 'true') : ?>
+            Swal.fire({
+                icon: 'success',
+                title: 'Bienvenido',
+                text: 'Registro exitoso',
+                showConfirmButton: false,
+                timer: 1300
+            }).then(function() {
+                window.location.href = 'sesion.php';
+            });
+        <?php endif; ?>
+    </script>            
+    <?php require_once './fragments/footer.php'; ?>
+</body>
+</html>
